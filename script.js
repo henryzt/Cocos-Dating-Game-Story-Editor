05ea7b3c1bbfc21955e9506c77d5e8882370b628
Henry Zhang <henryzhang9802@gmail.com>
2019-06-24 20:46:13 +0100

    Add side pannel

---
+++ script.js
@@ -0,0 +1,9 @@
+function openNav() {
+    document.getElementById("mySidenav").style.width = "40%";
+    document.getElementById("main").style.marginRight = "40%";
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("main").style.marginRight= "0";
+}
