432cba47fa6e52f209485d0d5196158be2b222fe
Henry Zhang <henryzhang9802@gmail.com>
2019-06-25 11:37:35 +0100

    Now can dynamically input card

--- script.js
+++ script.js
@@ -20,3 +20,66 @@ function createActionsHTML(action1, action2, action3) {
 }
 
 createActionsHTML("左滑选项", "右滑选项", "超时选项")
+
+
+var templatePack = [{
+                cardId: "01",
+                instructionText: "一小时后，你到达战场。电影院门外的女朋友好像在发脾气。",
+                cardText: null,
+                cardImage: null,
+                cardType: "swipe",
+                timeLimit: 0,
+                actions: [{
+                    order: 1,
+                    text: "“宝贝，我错了！”赶紧道歉",
+                    nextCardId: "02a",
+                    playerData: [-1,0,0,0]
+                },{
+                    order: 2,
+                    text: "“宝贝，谁欺负你了？”",
+                    nextCardId: "02c",
+                    playerData: [1,1,0,0]
+                },{
+                    order: 3,
+                    text: "沉默",
+                    nextCardId: "02b",
+                    playerData: [-2,0,0,0]
+                }]
+            }];
+
+
+function createCardHTML(cardObject) {
+    var template = document.getElementById("card_template").innerHTML;
+    var html = template.replace(/#cardId#/g, cardObject.cardId)
+                        .replace(/#instructionText#/g, cardObject.instructionText)
+                        .replace(/#cardText#/g, nullChecker(cardObject.cardText))
+                        .replace(/#cardImage#/g, nullChecker(cardObject.cardImage))
+                        .replace(/#cardType#/g, cardTypeSelector(cardObject.cardType))
+                        .replace(/#timeLimit#/g, cardObject.timeLimit == 0 ? "否":"是" );
+
+    document.getElementById("cards").innerHTML += html;
+}
+
+
+function cardTypeSelector(cardType){
+    switch(cardType){
+        case "swipe":
+            return "左右滑动";
+        case "chat":
+            return "微信聊天";
+        case "moments":
+            return "微信朋友圈";
+        case "fail":
+            return "游戏失败";
+        case "success":
+            return "游戏成功";
+        default:
+            return "未知";
+    }
+}
+
+function nullChecker(content){
+    return content? content : "无";
+}
+
+createCardHTML(templatePack[0])
