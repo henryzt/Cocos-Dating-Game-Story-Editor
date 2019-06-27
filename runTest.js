3dce31b3604979624b4a24981d2858b500cde538
Henry Zhang <henryzhang9802@gmail.com>
2019-06-27 15:40:19 +0100

    Add player value in test

--- runTest.js
+++ runTest.js
@@ -1,3 +1,4 @@
+var currentPlayerValue = [50,50,50,50]
 
 
 function setRunCard(id){
@@ -19,8 +20,10 @@ function setRunCard(id){
         console.log(nextCardId)
         if(nextCardId){
             let nextId = nextCardId.toString()
+            let valueChange = card.actions[i].playerData
             document.getElementById(`runAction${i+1}`).setAttribute("href", nextId)
             document.getElementById(`runAction${i+1}`).onclick = function(){
+                updatePlayerValue(valueChange)
                 setRunCard(nextId)
             }
         }else{
@@ -41,8 +44,27 @@ function setRunCard(id){
     }
 }
 
+function updatePlayerValue(valueChange){
+    for(let i = 0; i < 4; i++){
+        currentPlayerValue[i] = currentPlayerValue[i] + valueChange[i] * 10
+    }
+    console.log(currentPlayerValue)
+
+    document.querySelector('#pInterest').MaterialProgress.setProgress(currentPlayerValue[0]);
+    document.querySelector('#pLove').MaterialProgress.setProgress(currentPlayerValue[1]);
+    document.querySelector('#pWealth').MaterialProgress.setProgress(currentPlayerValue[2]);
+    document.querySelector('#pFamily').MaterialProgress.setProgress(currentPlayerValue[3]);
+
+    document.querySelector('#vInterest').innerHTML = (currentPlayerValue[0]);
+    document.querySelector('#vLove').innerHTML = (currentPlayerValue[1]);
+    document.querySelector('#vWealth').innerHTML = (currentPlayerValue[2]);
+    document.querySelector('#vFamily').innerHTML = (currentPlayerValue[3]);
+}
+
 
 function startNewRunTest(){
+    currentPlayerValue = [50,50,50,50]
+    updatePlayerValue([0,0,0,0])
     document.getElementById("runBlock").setAttribute("style","");
     document.getElementById("runHistory").innerHTML = ""
     setRunCard(cardsArray[0].cardId)
